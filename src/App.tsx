583847ba93fa2debac43c10f74486b5b18a68211
Emma Broman <emma.broman@liu.se>
2025-08-06 11:30:36 +0200

    Add some links and more information

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,15 @@ import {
   AppShell,
   Box,
   Center,
-  Container,
   createTheme,
+  Flex,
   Group,
   MantineProvider,
   NavLink,
-  Paper,
   Text,
 } from "@mantine/core";
 import "@mantine/core/styles.css";
+import { FrontPage } from "./pages/FrontPage";
 
 const theme = createTheme({
   fontFamily: "'Afacad Flux',  sans-serif",
@@ -24,37 +24,33 @@ export function App() {
     <MantineProvider theme={theme} defaultColorScheme="dark">
       <AppShell header={{ height: 60 }} padding="md">
         <AppShell.Header p="xs">
-          <Text
-            size="lg"
-            fw={"bold"}
-            variant="gradient"
-            gradient={{ from: "yellow", to: "forestgreen" }}
-            style={{
-              fontFamily: "'Arial', sans-serif",
-              position: "absolute",
-              top: "50%",
-              transform: "translateY(-50%)",
-            }}
-          >
-            Emma Broman
-          </Text>
-          <Center>
-            <Group>
-              <Box>
-                <NavLink label="Bio" />
-              </Box>
-              <Box>
-                <NavLink label="Publications" />
-              </Box>
-            </Group>
-          </Center>
+          <Flex>
+            <Flex w={150} align="center">
+              <Text
+                size="md"
+                fw={500}
+                c={"green"}
+                style={{ fontFamily: "Verdana" }}
+              >
+                Emma Broman
+              </Text>
+            </Flex>
+
+            <Center flex={1}>
+              <Group>
+                <Box>
+                  <NavLink label="Bio" />
+                </Box>
+                <Box>
+                  <NavLink label="Publications" />
+                </Box>
+              </Group>
+            </Center>
+            <Flex w={150} align="center" />
+          </Flex>
         </AppShell.Header>
-        <AppShell.Main bg="dark.9">
-          <Container p={"md"}>
-            <Paper p="md">
-              <Text>This page is a work in progress.</Text>
-            </Paper>
-          </Container>
+        <AppShell.Main bg="background">
+          <FrontPage />
         </AppShell.Main>
       </AppShell>
     </MantineProvider>
