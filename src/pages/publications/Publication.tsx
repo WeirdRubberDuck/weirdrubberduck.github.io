67a67a0da96dddf95c3e648341e06dc6d6b8610f
Emma Broman <emmabroman740@gmail.com>
2025-08-19 10:37:39 +0200

    Add abstracts to publications and make list interactive

--- src/pages/publications/Publication.tsx
+++ src/pages/publications/Publication.tsx
@@ -18,78 +18,76 @@ import { Fragment } from "react/jsx-runtime";
 
 export function Publication({ data }: { data: PublicationData }) {
   return (
-    <Card p={"md"} shadow="sm">
-      <Group wrap="nowrap" align="top">
-        <AspectRatio ratio={3 / 2} w={200} visibleFrom="sm">
-          <Image
-            src={data.thumbnail}
-            alt="Publication Image"
-            fallbackSrc={`https://placehold.co/600x400?text=${data.type}`}
-          />
-        </AspectRatio>
-        <Stack flex={1} gap={"xs"}>
-          <Group justify="space-between" align="top">
-            <Title order={3} flex={1} size={"lg"}>
-              {data.title}
-            </Title>
-            <Text>{data.year}</Text>
-          </Group>
-          <Text>
-            {data.authors.map((author, idx) => (
-              <Fragment key={author + idx}>
-                <Text span fw={author === "Emma Broman" ? 700 : undefined}>
-                  {author}
-                </Text>
-                {idx < data.authors.length - 1 && ", "}
-              </Fragment>
-            ))}
-          </Text>
-          <Text c="dimmed">{data.venue}</Text>
-          <Group justify="space-between" wrap="nowrap" align="end">
-            <Group gap={"xs"}>
-              {data.pdf && (
-                <Anchor href={data.doi} target="_blank">
-                  <Flex align={"center"} gap={4}>
-                    <FaRegFilePdf size={IconSize.xs} />
-                    PDF
-                  </Flex>
-                </Anchor>
-              )}
+    <Group wrap="nowrap" align="top">
+      <AspectRatio ratio={3 / 2} w={200} visibleFrom="sm">
+        <Image
+          src={data.thumbnail}
+          alt="Publication Image"
+          fallbackSrc={`https://placehold.co/600x400?text=${data.type}`}
+        />
+      </AspectRatio>
+      <Stack flex={1} gap={"xs"}>
+        <Group justify="space-between" align="top">
+          <Title order={3} flex={1} size={"lg"}>
+            {data.title}
+          </Title>
+          <Text>{data.year}</Text>
+        </Group>
+        <Text>
+          {data.authors.map((author, idx) => (
+            <Fragment key={author + idx}>
+              <Text span fw={author === "Emma Broman" ? 700 : undefined}>
+                {author}
+              </Text>
+              {idx < data.authors.length - 1 && ", "}
+            </Fragment>
+          ))}
+        </Text>
+        <Text c="dimmed">{data.venue}</Text>
+        <Group justify="space-between" wrap="nowrap" align="end">
+          <Group gap={"xs"}>
+            {data.pdf && (
+              <Anchor href={data.pdf} target="_blank">
+                <Flex align={"center"} gap={4}>
+                  <FaRegFilePdf size={IconSize.xs} />
+                  PDF
+                </Flex>
+              </Anchor>
+            )}
 
-              {data.bib && (
-                <Anchor href={data.bib} target="_blank" download>
-                  BibTeX
-                </Anchor>
-              )}
-              {data.code && (
-                <Anchor href={data.code} target="_blank">
-                  <Flex align={"center"} gap={4}>
-                    <FaCode size={IconSize.xs} />
-                    Code
-                  </Flex>{" "}
-                </Anchor>
-              )}
-              {data.doi && (
-                <Anchor href={data.doi} target="_blank">
-                  <Flex align={"center"} gap={4}>
-                    <SiDoi size={IconSize.xs} />
-                    DOI
-                  </Flex>
-                </Anchor>
-              )}
-              {data.website && (
-                <Anchor href={data.website} target="_blank">
-                  <Flex align={"center"} gap={4}>
-                    <FaCloud size={IconSize.xs} />
-                    Web
-                  </Flex>
-                </Anchor>
-              )}
-            </Group>
-            <Text style={{ whiteSpace: "nowrap" }}>{data.type}</Text>
+            {data.bib && (
+              <Anchor href={data.bib} target="_blank" download>
+                BibTeX
+              </Anchor>
+            )}
+            {data.code && (
+              <Anchor href={data.code} target="_blank">
+                <Flex align={"center"} gap={4}>
+                  <FaCode size={IconSize.xs} />
+                  Code
+                </Flex>{" "}
+              </Anchor>
+            )}
+            {data.doi && (
+              <Anchor href={data.doi} target="_blank">
+                <Flex align={"center"} gap={4}>
+                  <SiDoi size={IconSize.xs} />
+                  DOI
+                </Flex>
+              </Anchor>
+            )}
+            {data.website && (
+              <Anchor href={data.website} target="_blank">
+                <Flex align={"center"} gap={4}>
+                  <FaCloud size={IconSize.xs} />
+                  Web
+                </Flex>
+              </Anchor>
+            )}
           </Group>
-        </Stack>
-      </Group>
-    </Card>
+          <Text style={{ whiteSpace: "nowrap" }}>{data.type}</Text>
+        </Group>
+      </Stack>
+    </Group>
   );
 }
