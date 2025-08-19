55331b82fd4c238b223115645869631235c41944
Emma Broman <emmabroman740@gmail.com>
2025-08-19 10:02:26 +0200

    Update publication styles

--- src/util/types.ts
+++ src/util/types.ts
@@ -5,9 +5,12 @@ export interface PublicationData {
   type: "Paper" | "Short Paper" | "Poster" | "Review Paper" | "Abstract";
   year: number;
   date: string; // ISO date string
-  doi: string;
-  pdf: string;
-  bib: string;
-  thumbnail: string;
-  code: string;
+  pdf: string; // Link to pdf
+  doi?: string; // DOI link
+  bib?: string; // Link to bibtex file (for download)
+  thumbnail?: string; // Link to a thumbnail image
+  code?: string; // Link to code repository
+  website?: string; // Link to project website
+  poster?: string; // Link to poster PDF/image
+  video?: string; // Link to video
 }
