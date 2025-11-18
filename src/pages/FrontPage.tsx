b243dedc680821dd4b0ed86c05bf3df7957c76be
Emma Broman <emma.broman@liu.se>
2025-11-18 11:32:20 +0100

    Add some logos to the first page

--- src/pages/FrontPage.tsx
+++ src/pages/FrontPage.tsx
@@ -1,5 +1,13 @@
 import { ContactLinks } from "@components/ContactLinks";
-import { Anchor, Box, Container, Group, Paper, Text } from "@mantine/core";
+import {
+  Anchor,
+  Box,
+  Container,
+  Group,
+  Paper,
+  Text,
+  Image,
+} from "@mantine/core";
 
 export function FrontPage() {
   return (
@@ -28,13 +36,31 @@ export function FrontPage() {
         </Paper>
 
         <Paper p="md" flex={1} miw={200}>
-          <Text mb={"sm"}>About me</Text>
+          <Text mb={"sm"}>Contact</Text>
+          <Text c={"dimmed"}>Emma Broman</Text>
           <Text c={"dimmed"}>PhD Student and Research Engineer</Text>
           <Text c={"dimmed"}>Linköping University, Sweden</Text>
           <Box mt={"xs"}>
             <ContactLinks />
           </Box>
         </Paper>
+      </Group>
+
+      <Group justify="center" pt="lg">
+        <Anchor href="https://liu.se/en">
+          <Image
+            src="/images/LiU_primary_white.png"
+            alt="Linköping University Logo"
+            maw="256"
+          />
+        </Anchor>
+        <Anchor href="https://www.openspaceproject.com/">
+          <Image
+            src="/images/openspace-horiz-logo.png"
+            alt="OpenSpace Logo"
+            maw="256"
+          />
+        </Anchor>
       </Group>
     </Container>
   );
